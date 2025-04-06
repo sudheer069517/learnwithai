c775c5cf3ab6b81a488d9119e2eff295d2990424
sudheer kavuri <kavurisudheerkumar@gmail.com>
2025-04-06 12:54:14 +0530

    Initial commit

---
+++ src/main/java/com/teachgenie/learnwithai/LearnwithaiApplication.java
@@ -0,0 +1,13 @@
+package com.teachgenie.learnwithai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnwithaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnwithaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachgenie/learnwithai/LearnwithaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachgenie.learnwithai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnwithaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
